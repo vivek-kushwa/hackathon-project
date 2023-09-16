e61cf45176b3377a330cd5f3e36979755941b5d4
ravindramalvi21 <ravindra.kumar@antino.io>
2023-09-16 15:29:00 +0530

    Validation schema in sign up

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -2,8 +2,19 @@ import { Box, Paper, Stack } from "@mui/material";
 import { Button, Heading, TextField } from "components";
 import React from "react";
 import auth from "assets/images/auth.svg";
+import * as Yup from "yup";
+import { Form } from "layout";
 
 const SignUp = () => {
+  const validationSchema = Yup.object().shape({
+    firstName: Yup.string().required("Please Enter your first Name"),
+    lastName: Yup.string().required("Please enter your last name"),
+    email: Yup.string().email("Please enter your valid email"),
+    password: Yup.string()
+      .min(8, "Please enter your valid password")
+      .required("Please enter your password"),
+  });
+
   return (
     <>
       <Paper
@@ -24,45 +35,81 @@ const SignUp = () => {
         </Stack>
         <Stack spacing={5} m={10}>
           <Heading>Sign Up</Heading>
-          <Box>
-            <Stack spacing={3}>
-              <Stack direction={"row"} spacing={3}>
-                <TextField
-                  label={"Enter your name"}
-                  placeholder={"Enter your name"}
-                  isRequired={true}
-                  name={"firstName"}
-                />
+          <Form
+            handleSubmit={(data) => {}}
+            validationSchema={validationSchema}
+            initialValues={{
+              firstName: "",
+              lastName: "",
+              email: "",
+              password: "",
+            }}
+          >
+            {(formik) => {
+              const { errors, touched, values, handleChange } = formik;
+              return (
+                <>
+                  <Box>
+                    <Stack spacing={3}>
+                      <Stack direction={"row"} spacing={3}>
+                        <TextField
+                          label={"Enter your name"}
+                          placeholder={"Enter your name"}
+                          isRequired={true}
+                          name={"firstName"}
+                          onChange={handleChange}
+                          value={values?.firstName}
+                          error={touched?.firstName && !!errors?.firstName}
+                          errorMessage={errors?.firstName}
+                        />
 
-                <TextField
-                  label={"Enter your last name"}
-                  placeholder={"Enter your last name"}
-                  isRequired={true}
-                  name={"lastName"}
-                />
-              </Stack>
+                        <TextField
+                          label={"Enter your last name"}
+                          placeholder={"Enter your last name"}
+                          isRequired={true}
+                          name={"lastName"}
+                          onChange={handleChange}
+                          value={values?.lastName}
+                          error={touched?.lastName && !!errors?.lastName}
+                          errorMessage={errors?.lastName}
+                        />
+                      </Stack>
 
-              <Stack direction={"row"} spacing={3}>
-                <TextField
-                  label={"Enter your email"}
-                  placeholder={"Enter your email"}
-                  isRequired={true}
-                  name={"name"}
-                  type={"email"}
-                />
+                      <Stack direction={"row"} spacing={3}>
+                        <TextField
+                          label={"Enter your email"}
+                          placeholder={"Enter your email"}
+                          isRequired={true}
+                          name={"name"}
+                          type={"email"}
+                          onChange={handleChange}
+                          value={values?.email}
+                          error={touched?.email && !!errors?.email}
+                          errorMessage={errors?.email}
+                        />
 
-                <TextField
-                  label={"Enter your password"}
-                  placeholder={"Enter your password"}
-                  isRequired={true}
-                  name={"password"}
-                  type={"password"}
-                />
-              </Stack>
+                        <TextField
+                          label={"Enter your password"}
+                          placeholder={"Enter your password"}
+                          isRequired={true}
+                          name={"password"}
+                          type={"password"}
+                          onChange={handleChange}
+                          value={values?.password}
+                          error={touched?.password && !!errors?.password}
+                          errorMessage={errors?.password}
+                        />
+                      </Stack>
 
-              <Button variant={"contained"}>Log In</Button>
-            </Stack>
-          </Box>
+                      <Button type="submit" variant={"contained"}>
+                        Log In
+                      </Button>
+                    </Stack>
+                  </Box>
+                </>
+              );
+            }}
+          </Form>
         </Stack>
       </Paper>
     </>
